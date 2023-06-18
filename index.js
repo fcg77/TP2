d1f4abc08eed560877e8ed4000cc8099270a8d6b
fcg77 <castrofabian@gmail.com>
2023-06-18 03:28:10 -0300

    .

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 
-const cantidadEntradas = document.getElementById('cantidadEntrada')
+const cantidadEntrada = document.getElementById('cantidadEntrada')
 
 const botonCalcular = document.getElementById('botonCalcular')
 
@@ -10,28 +10,26 @@ const totalCompra = document.getElementById ('totalCompra')
 
 
 
+
 function resumen(){
 
-if(categoria.value ==="Estudiante"){
-    resultado.innerHTML = "Total $"+ cantidadEntradas * (200 * 0.2)
+
+if(categoria.value ==="estudiante"){
+    totalCompra.innerHTML = "Total $" + cantidadEntrada.value * (200 * 0.2)
     
 }
-if(categoria.value ==="Trainee"){
-    resultado.innerHTML = "Total $"+ cantidadEntradas * (200 * 0.5)
+if(categoria.value ==="trainee"){
+    totalCompra.innerHTML = "Total $" + cantidadEntrada.value * (200 * 0.5)
     
 }
-if(categoria.value ==="Junior"){
-    resultado.innerHTML = "Total $"+ cantidadEntradas * (200 * 0.85)
-    
+if(categoria.value ==="junior"){
+    totalCompra.innerHTML = "Total $" + cantidadEntrada.value * (200 * 0.85)
+
 }
 if(categoria.value===""){
     
-    resultado.innerHTML="Monto a Cobrar"
-}
+    totalCompra.innerHTML="Monto a Cobrar"
 }
 
-function reset(){
-    document.getElementById("cantidadEntrada").value = 0;
-    document.getElementById("totalCompra").innerHTML="";
-    document.getElementById("inputState").selectedIndex=0;
+
 }
